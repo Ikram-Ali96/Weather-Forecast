8bab924a696dbb9e69f17a19f817f86cf9f311f4
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-03-07 13:23:09 +0000

    Added basic search engine no API

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert ("hello world!");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let serarchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
